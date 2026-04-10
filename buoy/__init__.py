b26876da6b5a53b65d01fd1d986d8fc1299a61cb
William Benoit <benoi090@umn.edu>
2026-04-10 08:47:27 -0500

    Fix failing 3.10 unit tests

--- buoy/__init__.py
+++ buoy/__init__.py
@@ -1,5 +1,4 @@
-from buoy.main import main
 from buoy.models.aframe import Aframe, AframeConfig
 from buoy.models.amplfi import Amplfi, AmplfiConfig
 
-__all__ = ["Aframe", "AframeConfig", "Amplfi", "AmplfiConfig", "main"]
+__all__ = ["Aframe", "AframeConfig", "Amplfi", "AmplfiConfig"]
